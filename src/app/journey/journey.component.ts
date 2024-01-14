16a68a339d36a654541e416a255188103ef20265
Jaypatel2510 <pateljay251001@gmail.com>
2024-01-14 14:25:58 -0500

    Publish to Github

---
+++ src/app/journey/journey.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-journey',
+  templateUrl: './journey.component.html',
+  styleUrls: ['./journey.component.css']
+})
+export class JourneyComponent {
+
+}
